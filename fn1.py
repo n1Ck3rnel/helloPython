8930d88dec4f09a7be403ca1aeb93c363089668f
n1Ck3rnel <k3rnelmaster@gmail.com>
2020-12-10 09:13:07 +0900

    study

--- fn1.py
+++ fn1.py
@@ -8,3 +8,22 @@ def get_fruits():
 
 print(get_fruits()[0])
 
+
+def var_param(a, *vp):
+    print(type(vp))
+    print(a, len(vp), vp[len(vp) -1])
+
+var_param(1,2,3)
+
+def plus(a,b):
+    return a+b
+
+s = '3 + 4'
+if '+' in s:
+    a = s.split('+')
+    b = [ int(i.strip()) for i in a]
+    # print(b)
+    print(plus(b[0], b[1]))
+    
+
+
