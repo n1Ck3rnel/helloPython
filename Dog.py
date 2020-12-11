0f61ad0802c1e269fc71a391c357762e0e866d96
n1Ck3rnel <k3rnelmaster@gmail.com>
2020-12-11 10:35:32 +0900

    study

---
+++ Dog.py
@@ -0,0 +1,16 @@
+class Dog:
+    def __init__(self):
+        self.name = "Dog"
+        print("Dog was Born")
+
+    def speak(self):
+        print("YELP!", self.name)
+
+    def wag(self):
+        print("Dog's wag tail")
+
+    def __del__(self):
+        print("destroy!!")
+
+puddle = Dog()
+puddle.speak()
